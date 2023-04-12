6788a4de7c9b1c60377089da6bb599e66490f1f7
shanawazSDE <doccuekno1@gmail.com>
2023-04-12 13:46:22 +0530

    separation of A and B classes from Main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,24 +4,8 @@ import java.io.*;
 import java.lang.*;
 import java.util.*;
 
-class A
-{
 
-  public String meth ()
-  {
-    return "Invoking method from class A";
-  }
-  
-}
-class B extends A{
-    
-    public String meth ()
-  {
-    return "Method is overridden in Extendend class B";
-  }
-    
-   
-}
+
 
 
 public class Main
