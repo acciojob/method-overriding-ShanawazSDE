c9a758814e3604b9ac02e5e79b8ed16907623cc1
shanawazSDE <doccuekno1@gmail.com>
2023-04-12 14:09:40 +0530

    use static class of A and B

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,6 +10,26 @@ import java.util.*;
 
 public class Main
 {
+
+  public static class A
+  {
+
+    public String meth ()
+    {
+      return "Invoking method from class A";
+    }
+
+  }
+
+  public static class B extends A {
+
+    public String meth ()
+    {
+      return "Method is overridden in Extendend class B";
+    }
+
+
+  }
   public static void main (String[]args)
   {
     System.out.println (new B().meth ());
